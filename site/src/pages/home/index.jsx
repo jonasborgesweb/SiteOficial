32bea33d1f29168fccb1efcbcba24c4b30a69c54
jonasborgesweb <jonasborgesweb@gmail.com>
2020-04-05 13:48:24 -0300

    Finalizado a Section Service

--- site/src/pages/home/index.jsx
+++ site/src/pages/home/index.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 //Import Partial Home
 import Banner from './partial/banner'
 import About from './partial/about'
+import Service from './partial/service'
+import Portfolio from './partial/portfolio'
 import Testimonial from './partial/testimonial'
+import Blog from './partial/blog'
+import Contact from './partial/contact'
 
 class Home extends Component {
 
@@ -12,7 +16,11 @@ class Home extends Component {
             <>
                 <Banner />
                 <About />
+                <Service />
+                <Portfolio />
                 <Testimonial />
+                <Blog />
+                <Contact />
             </>
         )
     }
